f2b04ae5afcb766dd5007fe15aba97751adb3988
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-10-22 01:14:13 +0200

    Code clean up

--- background.js
+++ background.js
@@ -1,41 +1,19 @@
-function closeTab(tabid) {
-  chrome.tabs.remove(tabid, function() { });
-}
 
-function isDefaultVideoplayer(url) {  
-  const regex = /(https:\/\/www.youtube.com\/watch\?v=)/gm;
-  
-  let m;
-  let ret = false;
-  while ((m = regex.exec(url)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex.lastIndex) {
-      regex.lastIndex++;
-    }
-
-    m.forEach((match, groupIndex) => {
-      ret = true;
-    });
-  }
-  
-  return ret;
+function isDefaultVideoplayer(url) {
+  return /https:\/\/www\.youtube\.com\/watch\?v=/gm.test(url);
 }
 
 function getVideoID(url) {
-  const regex = /((?<defaultVideoPlayer>https:\/\/www\.youtube\.com\/watch\?v=)|(?<defaultVideoTrailings>\&t*\S*))|((?<embedVideoPlayer>https:\/\/www\.youtube\.com\/embed\/)|(?<OccaisonalEmbedVideoTrailings>\?si=*\S*))/gm;  
-  let output = String(url);
-  let m;
-  while ((m = regex.exec(url)) !== null) {
+  const regex = /(\S*((\?v\=)|(\&v\=)|(embed\/)))|\&=*\S*/gm;
+  let output = String(url), m;
+  while (m = regex.exec(url)) {
     // This is necessary to avoid infinite loops with zero-width matches
-    if (m.index === regex.lastIndex) {
-      regex.lastIndex++;
-    }
+    regex.lastIndex += m.index === regex.lastIndex ? 1 : 0;
     
     m.forEach((match, groupIndex) => {
       output = output.replace(match, '');
     });
   }
-  
   return output;
 }
 
@@ -43,82 +21,63 @@ function createEmbedURL(videoID) {
   return "https://www.youtube.com/embed/" + videoID;
 }
 
-async function processVideo(tabid, url) {
-  if (isDefaultVideoplayer(url)) {
-    // close video and open embed player, if not black listed
-    await chrome.storage.local.get(null).then(data => {
-      let contains = false;
-      for (var key in data) {
-        // if the video id is blacklisted and true
-        if (key === getVideoID(url) && data[key]) 
-          contains = true;
-      }        
-      if (!contains) {
-        closeTab(tabid, url);
-        let playerUrl = createEmbedURL(getVideoID(url));
-        chrome.tabs.create({ url: playerUrl });
-      }
-    });
-  }  
+async function processVideo(tabid, url) { 
+  if (!isDefaultVideoplayer(url) || await isBlacklisted(url)) return;
+  chrome.tabs.remove(tabid);
+  let playerUrl = createEmbedURL(getVideoID(url));
+  chrome.tabs.create({ url: playerUrl });
+}
+
+async function isBlacklisted(url) {
+  var output = false;
+  await chrome.storage.local.get(null).then(result => {
+    for (var key in result) 
+      if (key === getVideoID(url) && result[key]) {
+        output = true;
+        return;
+      }    
+  });
+  return output;
 }
 
 chrome.runtime.onMessage.addListener(async function(request) {  
   if (request.action.includes("blacklist")) {    
     let enabled = request.action.includes("true");
-    let vid = await getVideoID((await getLastValidTab()).url);
-    if (!vid) return;
+    let tab = await getCurrentTab();
+    if (!tab || !tab.url) return;
 
-    if (enabled) {
-      await chrome.storage.local.set({ [vid]:true });    
-    }
-    else {
-      await chrome.storage.local.remove([vid]);
-    }
+    let vid = getVideoID(tab.url); 
+    console.assert(vid);
 
-    printStorage();
+    if (enabled) await chrome.storage.local.set   ({[vid]:true});
+    else         await chrome.storage.local.remove( [vid]);    
   }
 });
 
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
   if (!changeInfo.url) return;      
+  console.assert(changeInfo.url);
   processVideo(tab.id, changeInfo.url);
 });
 
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
   if (request.action === "youtubeTabUpdate") {
-    chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
-      let tab = tabs[0];
-      if (!tab) return;
-      processVideo(tab.id, tab.url);
-    });
+    let tab = await getCurrentTab();
+    if (!tab || !tab.url) return;
+    processVideo(tab.id, tab.url);
   }
 });
 
 chrome.tabs.onUpdated.addListener(onTabUpdate);
 chrome.tabs.onActivated.addListener(onTabUpdate);
 
-async function getLastValidTab() {
-  let result;
-  await chrome.storage.local.get(["LAST_VALID_TAB"]).then(data => {
-    result = data.LAST_VALID_TAB;
-  });
-  return result;
-}
-async function onTabUpdate() {
+async function onTabUpdate() { }
 
-  let tab = await getCurrentTab();
-  if (tab && tab.url) {
-    await chrome.storage.local.set({LAST_VALID_TAB:tab});
-    console.log((await getLastValidTab()).url);
-  }
 
-  if (tab.url === undefined) throw new TypeError("`currentTab` is undefined!");
-}
-
+// Get the current active tab in the lastly focused window
 async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
+  let [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
+  console.assert(tab);
   return tab;
 }
 
--- popup.js
+++ popup.js
@@ -1,78 +1,70 @@
 
 
+async function addBlacklistEntry(url) {
+    let vid = document.createElement('li');
+    vid.className = "listOption";
+    vid.innerText = url;
+    document.getElementById('blacklist').appendChild(vid);
+}
+async function initiateBlacklistSwitch() {
+    await chrome.storage.local.get(null).then(async result => {
+        for (var key in result) {
+            if (key === getVideoID((await getCurrentTab()).url) && result[key]) 
+                toggle.checked = true;                
+        }
+    })
+}
+async function clearBlacklist() {
+    let list = document.getElementById('blacklist');
+    while ((list.firstChild)) {
+        list.removeChild(list.firstChild);
+    }
+}
+async function updateBlacklist() {
+    await clearBlacklist();
+    await fillBlacklist();
+}
+async function fillBlacklist() {
+    await chrome.storage.local.get(null).then(async result => {
+        for (var key in result) {
+            if (result[key]) {
+                addBlacklistEntry(key);
+            } 
+        }
+    });
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
-    const toggle = document.getElementById('blacklistSwitch');
     
-    toggle.addEventListener("click", async function(event) {
+    const blacklistSwitch = document.getElementById('blacklistSwitch');
+    blacklistSwitch.addEventListener("click", async function(event) {
         if (event.target.id === "blacklistSwitch") {            
-            chrome.runtime.sendMessage({ action:"blacklist" + String(toggle.checked) });
+            chrome.runtime.sendMessage({ action:"blacklist" + String(blacklistSwitch.checked) });
         }
     });
 
-    async function listVideo(url) {
-        let vid = document.createElement('li');
-        vid.className = "listOption";
-        vid.innerText = url;
-        document.getElementById('blacklist').appendChild(vid);
-    }
-
-    async function initiateBlackListSwitch() {
-        await chrome.storage.local.get(null).then(async result => {
-            for (var key in result) {
-                if (key === getVideoID((await getLastValidTab()).url) && result[key]) {
-                    toggle.checked = true;
-                }
-            }
-        })
-    }
-    async function clearBlacklist() {
-        let list = document.getElementById('blacklist');
-        while ((list.firstChild)) {
-            list.removeChild(list.firstChild);
-        }
-    }
-    async function updateBlackList() {
-        await chrome.storage.local.get(null).then(async result => {
-
-            await clearBlacklist();
-
-            for (var key in result) {
-                if (key === "LAST_VALID_TAB")
-                    continue;
-
-                if (result[key]) {
-                    listVideo(key);
-                } 
-            }
-        })
-    }
+    chrome.storage.onChanged.addListener(updateBlacklist);
     
-    initiateBlackListSwitch();
-    updateBlackList();
-    
-    chrome.storage.onChanged.addListener(updateBlackList);
+    initiateBlacklistSwitch();
+    updateBlacklist();
 });
 
 
-async function getLastValidTab() {
-    let result;
-    await chrome.storage.local.get(["LAST_VALID_TAB"]).then(data => {
-        result = data.LAST_VALID_TAB;
-    });
-    return result;
+// Get the current active tab in the lastly focused window
+async function getCurrentTab() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    console.assert(tab);
+    return tab;
 }
 
 
 function getVideoID(url) {
-    const regex = /((?<defaultVideoPlayer>https:\/\/www\.youtube\.com\/watch\?v=)|(?<defaultVideoTrailings>\&t*\S*))|((?<embedVideoPlayer>https:\/\/www\.youtube\.com\/embed\/)|(?<OccaisonalEmbedVideoTrailings>\?si=*\S*))/gm;  
-    let output = String(url);
-    let m;
-    while ((m = regex.exec(url)) !== null) {
+    const regex = /(\S*((\?v\=)|(\&v\=)|(embed\/)))|\&=*\S*/gm;
+    let output = String(url), m;
+    while (m = regex.exec(url)) {
         // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
-
+        regex.lastIndex += m.index === regex.lastIndex ? 1 : 0;
+        
         m.forEach((match, groupIndex) => {
             output = output.replace(match, '');
         });
