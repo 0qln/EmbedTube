95276e6ae0e67fcaecc68a84856a0d5a4b5930be
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-11-02 00:06:34 +0100

    Blacklist this site

--- GUI/Popup/popup.js
+++ GUI/Popup/popup.js
@@ -1,3 +1,5 @@
+import { getBlacklisted, setBlacklisted } from "../../blacklist.js";
+import { extractVideoID, getCurrentTab } from "../../utils.js";
 
 
 document.addEventListener("DOMContentLoaded", async function () {
@@ -5,4 +7,19 @@ document.addEventListener("DOMContentLoaded", async function () {
     btn.addEventListener('click', event => {
         chrome.tabs.create({url: '../GUI/Settings/settings.html'});
     });
+    initiateBlacklistSwitch();
 });
+
+
+async function initiateBlacklistSwitch() {    
+    // get
+    const value = await getBlacklisted(extractVideoID((await getCurrentTab()).url));
+    blacklistSwitch().checked = value ?? false;
+    // set
+    blacklistSwitch().addEventListener("click", async function(event) {
+        const id = extractVideoID((await getCurrentTab()).url);
+        setBlacklisted(id, blacklistSwitch().checked);
+    });
+}
+
+function blacklistSwitch() { return document.getElementById('blacklist-switch'); }
